90ff79d820a4309d854ac8f955d644c4d6bc3232
Jon Sagara <jon@sagarasoftware.com>
2023-08-01 11:04:24 -0700

    Implemented the client in F#.

---
+++ src/ClientFSharp/Program.fs
@@ -0,0 +1,28 @@
+﻿namespace ClientFSharp
+
+open System
+open System.Net
+open System.Net.Sockets
+
+module Program =
+
+    let private runClientAsync () = 
+        task {
+            use clientSocket = new Socket(SocketType.Stream, ProtocolType.Tcp)
+
+            printfn "Connecting to port 8087"
+
+            clientSocket.Connect(IPEndPoint(IPAddress.Loopback, 8087))
+            use stream = new NetworkStream(clientSocket)
+
+            do! Console.OpenStandardInput().CopyToAsync(stream)
+        }
+
+    [<EntryPoint>]
+    let main argv =
+        runClientAsync ()
+        |> Async.AwaitTask
+        |> Async.RunSynchronously
+
+        0
+
